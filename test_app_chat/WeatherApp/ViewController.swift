133e4561dbbb37bb0e20128daf9fcb3ee21a6baa
Ilya Sukhonosov <99659316+ilya-sukhonosov@users.noreply.github.com>
2022-10-31 22:35:51 +0300

    Add files via upload

---
+++ test_app_chat/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ilya Sukhonosov on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
